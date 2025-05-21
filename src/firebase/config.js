4273ba2064bd9b117227b67e1c5d95b9e009fbdc
Rohith S Moolya <smoolyarohith@gmail.com>
2025-05-21 17:11:03 +0530

    WTC COFFEE SHOP APP

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,17 +2,27 @@ import { initializeApp } from "firebase/app";
 import { initializeAuth, getReactNativePersistence } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import {
+  FIREBASE_API_KEY,
+  FIREBASE_AUTH_DOMAIN,
+  FIREBASE_PROJECT_ID,
+  FIREBASE_STORAGE_BUCKET,
+  FIREBASE_MESSAGING_SENDER_ID,
+  FIREBASE_APP_ID,
+  FIREBASE_MEASUREMENT_ID
+} from '@env';
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAWopSV8beuX9HY3GaezbCENx1RijJSaPE",
-  authDomain: "wtc-application-710fa.firebaseapp.com",
-  projectId: "wtc-application-710fa",
-  storageBucket: "wtc-application-710fa.firebasestorage.app",
-  messagingSenderId: "369861087422",
-  appId: "1:369861087422:web:fdb63d157c8f40ab6fceef",
-  measurementId: "G-B49SJXM861"
+  apiKey: FIREBASE_API_KEY,
+  authDomain: FIREBASE_AUTH_DOMAIN,
+  projectId: FIREBASE_PROJECT_ID,
+  storageBucket: FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+  appId: FIREBASE_APP_ID,
+  measurementId: FIREBASE_MEASUREMENT_ID
 };
+
+export default firebaseConfig;
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
