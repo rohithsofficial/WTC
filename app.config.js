6996a22803b92d677b7592b13e6a71268b215871
Sanmay Naik <sanmaynaik@Sanmays-MacBook-Air.local>
2025-06-25 19:36:33 +0530

    work on mac

--- app.config.js
+++ app.config.js
@@ -1,48 +1,45 @@
 import 'dotenv/config';
 
-export default {
-  expo: {
-    name: "WTC COFFEE SHOP",
-    slug: "react-native-firebase", // ✅ use kebab-case (no spaces/underscores)
-    platforms: ["ios", "android", "web"],
-    version: "1.0.0",
-    orientation: "portrait",
-    sdkVersion: "53.0.0",
-    icon: "./assets/icon.png",
-    splash: {
-      image: "./assets/splash.png",
-      resizeMode: "contain",
-      backgroundColor: "#ffffff"
-    },
-    updates: {
-      fallbackToCacheTimeout: 0
-    },
-    assetBundlePatterns: ["**/*"],
-    ios: {
-       bundleIdentifier: "com.rohithofficial.wtccoffee",
-      supportsTablet: true,
-      infoPlist: {
-        NSPhotoLibraryUsageDescription: "Allow $(PRODUCT_NAME) to access your photos to let you share them with your friends.",
-        NSCameraUsageDescription: "Allow $(PRODUCT_NAME) to access your camera to let you take photos for your profile."
-      }
-    },
-    android: {
-      // "adaptiveIcon": {
-      //   "foregroundImage": "./assets/adaptive-icon.png",
-      //   "backgroundColor": "#FFFFFF"
-      // },
-      permissions: [
-        "CAMERA",
-        "READ_EXTERNAL_STORAGE",
-        "WRITE_EXTERNAL_STORAGE",
-        "android.permission.CAMERA",
-        "android.permission.WRITE_SETTINGS",
-        "android.permission.SYSTEM_ALERT_WINDOW",
-        "android.permission.WAKE_LOCK"
-      ],
-      package: "com.rohithofficial.wtccoffee",
-    },
-    plugins: [
+export default ({ config }) => ({
+  ...config,
+  entryPoint: './app/index.tsx',
+  name: "WTC COFFEE SHOP",
+  slug: "react-native-firebase",
+  version: "1.0.0",
+  orientation: "portrait",
+  sdkVersion: "53.0.0",
+  platforms: ["ios", "android", "web"],
+  icon: "./assets/icon.png",
+  splash: {
+    image: "./assets/splash.png",
+    resizeMode: "contain",
+    backgroundColor: "#ffffff"
+  },
+  updates: {
+    fallbackToCacheTimeout: 0
+  },
+  assetBundlePatterns: ["**/*"],
+  ios: {
+    bundleIdentifier: "com.rohithofficial.wtccoffee",
+    supportsTablet: true,
+    infoPlist: {
+      NSPhotoLibraryUsageDescription: "Allow $(PRODUCT_NAME) to access your photos to let you share them with your friends.",
+      NSCameraUsageDescription: "Allow $(PRODUCT_NAME) to access your camera to let you take photos for your profile."
+    }
+  },
+  android: {
+    permissions: [
+      "CAMERA",
+      "READ_EXTERNAL_STORAGE",
+      "WRITE_EXTERNAL_STORAGE",
+      "android.permission.CAMERA",
+      "android.permission.WRITE_SETTINGS",
+      "android.permission.SYSTEM_ALERT_WINDOW",
+      "android.permission.WAKE_LOCK"
+    ],
+    package: "com.rohithofficial.wtccoffee"
+  },
+  plugins: [
   "expo-router",
   "expo-barcode-scanner",
   [
@@ -55,28 +52,40 @@ export default {
   [
     "expo-brightness",
     {
-      "permissions": ["WRITE_SETTINGS"]
+      permissions: ["WRITE_SETTINGS"]
+    }
+  ],
+  [
+    "expo-build-properties",
+    {
+      android: {
+        compileSdkVersion: 35,
+        targetSdkVersion: 35,
+        buildToolsVersion: "35.0.0",
+        minSdkVersion: 24
+      },
+      ios: {
+        deploymentTarget: "15.1"
+      }
     }
   ]
 ],
-
-    scheme: "wtc",
-    extra: {
-      eas: {
-        projectId: "ef749917-19c7-4e5b-992f-99d37805c8d7",
-      },
-      firebaseApiKey: process.env.FIREBASE_API_KEY,
-      firebaseAuthDomain: process.env.FIREBASE_AUTH_DOMAIN,
-      firebaseProjectId: process.env.FIREBASE_PROJECT_ID,
-      firebaseStorageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-      firebaseMessagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-      firebaseAppId: process.env.FIREBASE_APP_ID,
-      firebaseMeasurementId: process.env.FIREBASE_MEASUREMENT_ID,
-      phonepeTestMerchantId: process.env.PHONEPE_TEST_MERCHANT_ID,
-      phonepeTestSaltKey: process.env.PHONEPE_TEST_SALT_KEY,
-      phonepeLiveMerchantId: process.env.PHONEPE_LIVE_MERCHANT_ID,
-      phonepeLiveSaltKey: process.env.PHONEPE_LIVE_SALT_KEY,
-      phonepeSaltIndex: process.env.PHONEPE_SALT_INDEX,
-    }
+  scheme: "wtc",
+  extra: {
+    eas: {
+      projectId: "ef749917-19c7-4e5b-992f-99d37805c8d7"
+    },
+    firebaseApiKey: process.env.FIREBASE_API_KEY,
+    firebaseAuthDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    firebaseProjectId: process.env.FIREBASE_PROJECT_ID,
+    firebaseStorageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    firebaseMessagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    firebaseAppId: process.env.FIREBASE_APP_ID,
+    firebaseMeasurementId: process.env.FIREBASE_MEASUREMENT_ID,
+    phonepeTestMerchantId: process.env.PHONEPE_TEST_MERCHANT_ID,
+    phonepeTestSaltKey: process.env.PHONEPE_TEST_SALT_KEY,
+    phonepeLiveMerchantId: process.env.PHONEPE_LIVE_MERCHANT_ID,
+    phonepeLiveSaltKey: process.env.PHONEPE_LIVE_SALT_KEY,
+    phonepeSaltIndex: process.env.PHONEPE_SALT_INDEX
   }
-};
+});
